5d4f440a832d9916c3e86c9e1f02fe4c1934028b
russell wright <russguy@gmail.com>
2019-05-05 14:33:47 -0700

    Added "Great" and "Ultimate" Robot Master weaknesses, in addition to Primary and Secondary. close #129

--- MM2RandoLib/Enums/EDmgVsBoss.cs
+++ MM2RandoLib/Enums/EDmgVsBoss.cs
@@ -14,29 +14,34 @@ namespace MM2Randomizer.Enums
             get; private set;
         }
 
+        public int Index
+        {
+            get; private set;
+        }
+
         public static Dictionary<int, EDmgVsBoss> Addresses;
 
         //Japanese
-        public static readonly EDmgVsBoss Buster          = new EDmgVsBoss(0x02E933, "Buster");
-        public static readonly EDmgVsBoss AtomicFire      = new EDmgVsBoss(0x02E941, "Atomic Fire");
-        public static readonly EDmgVsBoss AirShooter      = new EDmgVsBoss(0x02E94F, "Air Shooter");
-        public static readonly EDmgVsBoss LeafShield      = new EDmgVsBoss(0x02E95D, "Leaf Shield");
-        public static readonly EDmgVsBoss BubbleLead      = new EDmgVsBoss(0x02E96B, "Bubble Lead");
-        public static readonly EDmgVsBoss QuickBoomerang  = new EDmgVsBoss(0x02E979, "Quick Boomerang");
-        public static readonly EDmgVsBoss TimeStopper     = new EDmgVsBoss(0x02C049, "Time Stopper");
-        public static readonly EDmgVsBoss MetalBlade      = new EDmgVsBoss(0x02E995, "Metal Blade");
-        public static readonly EDmgVsBoss ClashBomber     = new EDmgVsBoss(0x02E987, "Clash Bomber");
+        public static readonly EDmgVsBoss Buster          = new EDmgVsBoss(0, 0x02E933, "Buster");
+        public static readonly EDmgVsBoss AtomicFire      = new EDmgVsBoss(1, 0x02E941, "Atomic Fire");
+        public static readonly EDmgVsBoss AirShooter      = new EDmgVsBoss(2, 0x02E94F, "Air Shooter");
+        public static readonly EDmgVsBoss LeafShield      = new EDmgVsBoss(3, 0x02E95D, "Leaf Shield");
+        public static readonly EDmgVsBoss BubbleLead      = new EDmgVsBoss(4, 0x02E96B, "Bubble Lead");
+        public static readonly EDmgVsBoss QuickBoomerang  = new EDmgVsBoss(5, 0x02E979, "Quick Boomerang");
+        public static readonly EDmgVsBoss TimeStopper     = new EDmgVsBoss(6, 0x02C049, "Time Stopper");
+        public static readonly EDmgVsBoss MetalBlade      = new EDmgVsBoss(7, 0x02E995, "Metal Blade");
+        public static readonly EDmgVsBoss ClashBomber     = new EDmgVsBoss(8, 0x02E987, "Clash Bomber");
         
         //English
-        public static readonly EDmgVsBoss U_DamageP = new EDmgVsBoss(0x2e952, "Buster");
-        public static readonly EDmgVsBoss U_DamageH = new EDmgVsBoss(0x2e960, "Atomic Fire");
-        public static readonly EDmgVsBoss U_DamageA = new EDmgVsBoss(0x2e96e, "Air Shooter");
-        public static readonly EDmgVsBoss U_DamageW = new EDmgVsBoss(0x2e97c, "Leaf Shield");
-        public static readonly EDmgVsBoss U_DamageB = new EDmgVsBoss(0x2e98a, "Bubble Lead");
-        public static readonly EDmgVsBoss U_DamageQ = new EDmgVsBoss(0x2e998, "Quick Boomerang");
-        public static readonly EDmgVsBoss U_DamageF = new EDmgVsBoss(0x2C049, "Time Stopper");
-        public static readonly EDmgVsBoss U_DamageM = new EDmgVsBoss(0x2e9b4, "Metal Blade");
-        public static readonly EDmgVsBoss U_DamageC = new EDmgVsBoss(0x2e9a6, "Clash Bomber");
+        public static readonly EDmgVsBoss U_DamageP = new EDmgVsBoss(0, 0x2e952, "Buster");
+        public static readonly EDmgVsBoss U_DamageH = new EDmgVsBoss(1, 0x2e960, "Atomic Fire");
+        public static readonly EDmgVsBoss U_DamageA = new EDmgVsBoss(2, 0x2e96e, "Air Shooter");
+        public static readonly EDmgVsBoss U_DamageW = new EDmgVsBoss(3, 0x2e97c, "Leaf Shield");
+        public static readonly EDmgVsBoss U_DamageB = new EDmgVsBoss(4, 0x2e98a, "Bubble Lead");
+        public static readonly EDmgVsBoss U_DamageQ = new EDmgVsBoss(5, 0x2e998, "Quick Boomerang");
+        public static readonly EDmgVsBoss U_DamageF = new EDmgVsBoss(6, 0x2C049, "Time Stopper");
+        public static readonly EDmgVsBoss U_DamageM = new EDmgVsBoss(7, 0x2e9b4, "Metal Blade");
+        public static readonly EDmgVsBoss U_DamageC = new EDmgVsBoss(8, 0x2e9a6, "Clash Bomber");
 
         static EDmgVsBoss()
         {
@@ -54,10 +59,11 @@ namespace MM2Randomizer.Enums
             };
         }
 
-        private EDmgVsBoss(int address, string name)
+        private EDmgVsBoss(int index, int address, string name)
         {
             this.Address = address;
             this.WeaponName = name;
+            this.Index = index;
         }
 
         public static implicit operator int (EDmgVsBoss eDmgVsBoss)

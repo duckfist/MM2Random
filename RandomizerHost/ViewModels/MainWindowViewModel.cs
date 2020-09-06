031487191123e45b3b88f5168cbb281b4e375614
squidman <squidman@outlook.com>
2020-09-06 15:05:29 -0700

    Initial Avalonia project.

---
+++ RandomizerHost/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Avalonia;
+using MM2Randomizer;
+using MM2Randomizer.Utilities;
+using ReactiveUI;
+
+namespace RandomizerHost.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        //
+        // Constructor
+        //
+
+        public MainWindowViewModel()
+        {
+            RandoSettings = new RandoSettings();
+            RandomMM2.Settings = RandoSettings;
+
+            // Try to load "MM2.nes" if one is in the local directory already to save time
+            String tryLocalpath = Path.Combine(
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                "MM2.nes");
+
+            if (File.Exists(tryLocalpath))
+            {
+                RandoSettings.ValidateFile(tryLocalpath);
+                IsShowingHint = false;
+            }
+        }
+
+
+        //
+        // Properties
+        //
+
+        public RandoSettings RandoSettings
+        {
+            get => this.mRandoSettings;
+            set => this.RaiseAndSetIfChanged(ref this.mRandoSettings, value);
+        }
+
+        public bool IsShowingHint
+        {
+            get => this.mIsShowingHint;
+            set => this.RaiseAndSetIfChanged(ref this.mIsShowingHint, value);
+        }
+
+        public bool HasGeneratedAROM
+        {
+            get => this.mHasGeneratedAROM;
+            set => this.RaiseAndSetIfChanged(ref this.mHasGeneratedAROM, value);
+        }
+
+        public bool IsCoreModulesChecked
+        {
+            get => RandoSettings.Is8StagesRandom &&
+                   RandoSettings.IsWeaponsRandom &&
+                   RandoSettings.IsTeleportersRandom;
+        }
+
+
+        //
+        // Public Methods
+        //
+
+        public void PerformRandomization(int seed, bool tryCreateLogFile)
+        {
+            // Perform randomization based on settings, then generate the ROM.
+            RandomMM2.RandomizerCreate(true, seed);
+
+            // Get A-Z representation of seed
+            String seedAlpha = SeedConvert.ConvertBase10To26(RandomMM2.Seed);
+
+            if (seed < 0)
+            {
+                RandoSettings.SeedString = seedAlpha;
+            }
+            Debug.WriteLine("\nSeed: " + seedAlpha + "\n");
+
+            // Create log file if left shift is pressed while clicking
+            if (tryCreateLogFile && !RandoSettings.IsSpoilerFree)
+            {
+                string logFileName = $"MM2RNG-{seedAlpha}.log";
+                using (StreamWriter sw = new StreamWriter(logFileName, false))
+                {
+                    sw.WriteLine("Mega Man 2 Randomizer");
+                    sw.WriteLine($"Version {RandoSettings.AssemblyVersion.ToString()}");
+                    sw.WriteLine($"Seed {seedAlpha}\n");
+                    sw.WriteLine(RandomMM2.randomStages.ToString());
+                    sw.WriteLine(RandomMM2.randomWeaponBehavior.ToString());
+                    sw.WriteLine(RandomMM2.randomEnemyWeakness.ToString());
+                    sw.WriteLine(RandomMM2.randomWeaknesses.ToString());
+                    sw.Write(RandomMM2.Patch.GetStringSortedByAddress());
+                }
+            }
+
+            // Flag UI as having created a ROM, enabling the "open folder" button
+            HasGeneratedAROM = true;
+        }
+
+
+        //
+        // Private Data Members
+        //
+
+        private RandoSettings mRandoSettings;
+        private bool mIsShowingHint = true;
+        private bool mHasGeneratedAROM = false;
+    }
+}
---
+++ RandomizerHost/Views/MainWindow.axaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
+
+namespace RandomizerHost.Views
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public async void BrowseButton_OnClick(Object in_Sender, RoutedEventArgs in_Args)
+        {
+            if (in_Sender is Button button && button.GetVisualRoot() is Window window)
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+
+                openFileDialog.AllowMultiple = false;
+
+                openFileDialog.Filters.Add(
+                    new FileDialogFilter()
+                    {
+                        Name = @"ROM Image",
+                        Extensions = new List<String>()
+                        {
+                            @"nes"
+                        }
+                    });
+
+                openFileDialog.Title = @"Open Mega Man 2 (U) NES ROM File";
+
+                // Call the ShowDialog method to show the dialog box.
+                String exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                String exeDir = Path.GetDirectoryName(exePath);
+                openFileDialog.Directory = exeDir;
+
+                String[] dialogResult = await openFileDialog.ShowAsync(window);
+
+                // Process input if the user clicked OK.
+                if (dialogResult.Length > 0)
+                {
+                    //TryFile(dlg.FileName);
+                    //SetTextBoxFocusToEnd();
+                }
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
